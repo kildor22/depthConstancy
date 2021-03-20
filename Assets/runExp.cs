3ad7410188bd8b4362038f7bc7e4936a823594d2
Cyan <Cybernide@users.noreply.github.com>
2021-03-20 16:20:47 -0400

    Random reisizing of reference object

--- Assets/runExp.cs
+++ Assets/runExp.cs
@@ -13,7 +13,8 @@ public class runExp : MonoBehaviour
     public string participantNo;
 
     public GameObject stimObj;
-    public int adjLength;
+    public GameObject refObj;
+    public float adjLength;
 
     // Trial duration calculations
     public System.TimeSpan trialDuration;
@@ -39,19 +40,15 @@ public class runExp : MonoBehaviour
         participantNo = "00";
 
         //Put reference into environment
-        GameObject refObj = Instantiate(mdl, new Vector3(500f, 1.36144f, 500.8f), 
-            Quaternion.identity);
+        InstantiateReference();
 
         // Put stimuli into environment
         InstantiateStimuli();
-
         float meshSz = stimObj.GetComponent<MeshFilter>().mesh.bounds.size.z;
-        Debug.Log(meshSz);
+
         // Start trial and get sys time for trial duration record
         trialStartTime = System.DateTime.Now;
         isTrial = true;
-
-        Debug.Log(AbsSz(stimObj));
         
     }
     void Update()
@@ -62,7 +59,6 @@ public class runExp : MonoBehaviour
             // User confirms their manipulation
             if (Input.GetKeyDown("space"))
             {
-                Debug.Log("space pressed");
                 OutputTrialResults();
                 isTrial = false;
                 Destroy(stimObj);
@@ -70,14 +66,12 @@ public class runExp : MonoBehaviour
             // Scale stim up
             else if (Input.GetKeyDown("up"))
             {
-                Debug.Log("up pressed");
                 AdjLen(stimObj, 0.01f);
                 
             }
             // Scale stim down
             else if (Input.GetKeyDown("down"))
             {
-                Debug.Log("down pressed");
                 AdjLen(stimObj, -0.01f);
             }
         }
@@ -96,6 +90,7 @@ public class runExp : MonoBehaviour
                 
                 isTrial = true;
                 adjLength = 0;
+                InstantiateReference();
                 InstantiateStimuli();
             }
         }
@@ -104,19 +99,20 @@ public class runExp : MonoBehaviour
     void OutputTrialResults()
     {
         // dummy variables for testing
-        float stimLen = AbsSz(stimObj);
-        Debug.Log(stimLen);
-        //Debug.Log(stim.
+        float refLen = AbsoluteSize(refObj);
+        Debug.Log(refLen);
+        float adjLen = AbsoluteSize(stimObj);
+        Debug.Log(adjLen);
         
         // Find trial duration
         trialEndTime = System.DateTime.Now;
-        trialDuration = trialStartTime - trialEndTime;
+        trialDuration = trialEndTime - trialStartTime;
 
         // Record trial results, output to file
         string trialResponses = 
-            (stimLen.ToString() + ',' + stimAzi.ToString() + ',' +
+            (refLen.ToString() + ',' + stimAzi.ToString() + ',' +
             stimEle.ToString() + ',' + trialDuration.ToString() + ',' + 
-            adjLength.ToString() + Environment.NewLine);
+            adjLenh.ToString() + Environment.NewLine);
 
         using (StreamWriter resultStream = File.AppendText(Application.dataPath +
             "/results/p_" + participantNo + ".txt"))
@@ -124,19 +120,7 @@ public class runExp : MonoBehaviour
             resultStream.Write(trialResponses);
         }
 
-        Debug.Log(trialResponses);
 
-    }
-
-    void CalcObjDisplacement()
-    {
-        // linear or radial displacement
-        // random coinflip selection of radial vs linear
-        // if linear:
-            // calc displacement
-        // if radial
-            // calc displacement
-        //
     }
 
     void InstantiateStimuli()
@@ -145,12 +129,19 @@ public class runExp : MonoBehaviour
         stimAzi = RandVal(500.5f, 499.3f);
         stimEle = RandVal(0.95f, 1.78f);
 
-        // TODO: actually would like to change these into angles rather than
-        // point-coordinates
-
-        stimObj = (GameObject)Instantiate(mdl, new Vector3(stimAzi, stimEle, 500.8f),
-           Quaternion.identity);
+        // TODO: change these into angles rather than point-coordinates
+        stimObj = (GameObject)Instantiate(mdl, 
+            new Vector3(stimAzi, stimEle, 500.8f), Quaternion.identity);
         
+    }
+
+    void InstantiateReference()
+    {
+        refObj = Instantiate(mdl, new Vector3(500f, 1.36144f, 500.8f),
+                    Quaternion.identity);
+        refObj.transform.localScale = new Vector3(1, 1,
+            RandVal(0.5f, 1.5f));
+
     }
 
     float RandVal(float max, float min) 
@@ -161,7 +152,7 @@ public class runExp : MonoBehaviour
         return (float) val;
     }
 
-    float AbsSz(GameObject go)
+    float AbsoluteSize(GameObject go)
     {
         //TODO: Debug
         // Calculate absolute length of object
@@ -172,10 +163,15 @@ public class runExp : MonoBehaviour
 
     void AdjLen(GameObject go, float adj)
     {
-        // Adjust the length of game object go by factor adj
+        // Adjust the length of game object go by adj
+        // Given that absolute size is given by mesh bounds * local scale
+        // Adding a 1m in Unity coordinates to the size of an object is
+        // 1m/mesh bounds + to the local scale
+
         float meshSz = go.GetComponent<MeshFilter>().mesh.bounds.size.z;
         var trScl = go.transform.localScale.z;
-        go.transform.localScale = new Vector3(1, 1, trScl+adj/meshSz); // change its local scale in x y z format
+        // change its local scale
+        go.transform.localScale = new Vector3(1, 1, trScl+adj/meshSz);
     }
 
 
